1210cc779d8ad4bb38ad6c8f5e131761e02be770
watakin <sbag0793@gmail.com>
2024-05-23 09:11:01 +0900

    UPDATE (nothing special)

---
+++ system/static/script.js
@@ -0,0 +1,7 @@
+// document.getElementById('studentButton').addEventListener('click', function() {
+//   window.location.href = '/register/student';
+// });
+
+// document.getElementById('teacherButton').addEventListener('click', function() {
+//   window.location.href = '/register/teacher';
+// });
--- system/views/regist_views.py
+++ system/views/regist_views.py
@@ -9,13 +9,30 @@ from system.models import Student, Teacher
 bp = Blueprint('register', __name__, url_prefix='/register')
 
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/')
 def index():
+  return redirect('register.student')
+
+@bp.route('/student', methods=('GET', 'POST'))
+def student():
   form = StudentForm()
+  student_list = Student.query.order_by(Student.id.desc()).all()
+
   if request.method == 'POST' and form.validate_on_submit():
     student = Student(grade=form.grade.data, Class=form.Class.data, number=form.number.data, name=form.name.data)
     db.session.add(student)
     db.session.commit()
-    return redirect(url_for('register.index'), code=302)
-  student_list = Student.query.order_by(Student.id.desc()).all()
+    return redirect(url_for('register.student'))
   return render_template('register/student_register.html', form=form, student_list=student_list)
+
+@bp.route('/teacher', methods=('GET', 'POST'))
+def teacher():
+  form = TeacherForm()
+  teacher_list = Student.query.order_by(Teacher.id.desc()).all()
+
+  if request.method == 'POST' and form.validate_on_submit():
+    teacher = Teacher(id=form.id.data, name=form.name.data, password=form.password.data, point=form.point.data)
+    db.session.add(teacher)
+    db.session.commit()
+    return redirect(url_for('register.teacher'))
+  return render_template('register/teacher_register.html', form=form, teacher_list=teacher_list)
